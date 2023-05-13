d701ef4c30a9461cd17c61803280357c9ebde354
Lovis Tawiah <69266504+lovistawiah@users.noreply.github.com>
2023-05-13 16:31:30 +0000

    updated the server to send and receive message from the client using different tabs as clients

--- public/js/index.js
+++ public/js/index.js
@@ -1,45 +1,64 @@
-// var socket = io();
-let form = document.getElementById('form');
-let message = document.getElementById('message');
-let username = document.getElementById('username');
-var messages = document.getElementById('messages');
-const allChat = []
+var socket = io();
+const form = document.getElementById('form');
+const message = document.getElementById('message');
+const username = document.getElementById('username');
+const messages = document.getElementById('messages');
+const indicator = document.getElementById("indicator");
+const connectionList = document.getElementById("connection")
 const messageItem = document.createElement("li")
 const span = document.createElement("span")
+
 messageItem.classList.add("message-item")
-messageItem.textContent = "helflfjdjfldfadhfejee"
-span.textContent = "-username"
-messageItem.appendChild(span)
-console.log(messageItem)
-messages.appendChild(messageItem)
-// form.addEventListener('submit', function (e) {
-//     e.preventDefault();
-//     if (message.value && username.value) {
-//         const userMessage = {
-//             username,
-//             message
-//         }
-//         socket.emit('chat message', userMessage);
-//         input.value = '';
-//     }
-// })
+const allChat = []
 
 
+function submitForm() {
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        if (message.value && username.value) {
+            const userMessage = {
+                "username": username.value,
+                "message": message.value,
+                "date": Date.now(),
+                "socket": socket.id
+            }
+            // ? chatting channel
+            socket.emit('chat message', userMessage);
+            message.value = '';
+        }
+    })
+}
+username.addEventListener("blur", () => {
+    let value = ""
+    value = username.value
+    message.addEventListener("focusin", (e) => {
+        socket.emit('typing', { "message": "typing...", "username": value })
+    })
+})
+
+socket.on("typing:message", (message) => {
+    messageItem.textContent = message.message
+    span.textContent = "-" + message.username
+    messageItem.appendChild(span)
+    messages.appendChild(messageItem)
+})
+
+socket.on("connect", () => {
+    indicator.textContent = "🟢"
+})
+
+socket.on("list", (connectionItems) => {
+    connectionList.textContent = connectionItems
+})
 
 
-// socket.on("connect", () => {
-//     socket.emit("status:", `${socket.id} connected`)
-// })
+socket.on("chat", (message) => {
+    messageItem.textContent = message.message
+    span.textContent = "-" + message.username
+    messageItem.appendChild(span)
+    messages.appendChild(messageItem)
 
-// input.addEventListener("focusin", () => {
-//     item.textContent ='typing..'
-//     messages.appendChild(item)
-// })
+})
 
-// socket.on("chat", (msg) => {
-//     const item = document.createElement("li")
-//     item.textContent = msg;
-//     messages.appendChild(item)
-// })
-
+submitForm()
 
--- server.js
+++ server.js
@@ -1,32 +1,80 @@
 const http = require("http")
 const { Server } = require("socket.io")
 const express = require("express");
-const fs = require("fs")
+
 
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
+
 app.use('/public', express.static("public/"))
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
-
+let connectionList = []
+let receivers = []
 
 io.on("connection", (socket) => {
-    socket.on("status:", (message) => {
-        console.log(message)
-    })
+    // ? add number of sockets 
+    connectionList.push(socket)
+
+    // ? returns connected sockets
+
+    console.log(socket.id + " connected")
+    const message = {
+        "message": `${socket.id} connected`,
+        "username": "Admin",
+    }
+    // sending connected sockets to all users
+    io.emit("chat", message)
+
+    // ? sending number of connection
+    io.emit("list", connectionList.length)
+
     //? fire when socket disconnected 
     socket.on("disconnect", () => {
         console.log(`${socket.id} disconnected`)
+        connectionList = connectionList.filter(s => s.id !== socket.id)
+
+        // send message to users when a disconnects
+        const message = {
+            "message": `${socket.id} disconnected`,
+            "username": "Admin",
+        }
+        io.emit("chat", message)
     })
 })
-
 io.on("connection", (socket) => {
+    // ? chatting channel
     socket.on("chat message", (userMessage) => {
-        console.log(userMessage)
-        socket.broadcast.emit("chat", userMessage)
+        // sending message to all users with my name excluding me
+        receivers = connectionList.filter(sock => sock.id !== socket.id)
+        receivers.forEach((sock) => {
+            sock.emit("chat", userMessage)
+        })
+        // ? sending to sender using me as `username` on the userMessage obj
+        let senderName = userMessage.username = "Me"
+        let sender = { ...userMessage, senderName }
+        socket.emit("chat", sender)
+
     })
+
+io.on("connection",(socket)=>{
+    // sending typing functionality
+    socket.on('typing', (userMessage) => {
+        console.log(userMessage)
+        // remove user 
+        receivers = connectionList.filter(sock => sock.id !== socket.id)
+        receivers.forEach(sock => {
+            console.log(sock.id)
+            sock.emit("typing:message",
+                {
+                    message: userMessage.message,
+                    username: userMessage.username
+                })
+        })
+    })
+})
 })
 
 
